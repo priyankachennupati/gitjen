9536db2b61c9fc071075977b5e7e008e7465b654
priyankachennupati <priyankachennupati20@gmail.com>
2018-08-01 13:14:51 +0530

    all file in deploy

---
+++ enviornment.java
@@ -0,0 +1,13 @@
+#!/bin/bash
+ENVIRONMENT=$i
+if [ $ENVIRONMENT = "QA" ]
+then
+for i in 'cat IPs txt'
+do
+  echo "Deploying war file into $i QA server.."
+  sleep 3
+  sshpass -p "priya" scp target/ola.war priya@$i:/home/priya/softwares/apache-tomcat-7.0.88/webapps
+  echo "starting tomcat server in $i QA server.."
+  sshpass -p "priya" ssh priya@$i "JAVA_HOME=/home/priya/softwares/jdk1.8.0_172" "/home/priya/softwares/apache-tomcat-7.0.88/bin/./startup.sh"
+done
+ echo "deploy success"
