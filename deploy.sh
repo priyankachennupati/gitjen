36c687871110429517e6580c3636f83d7c0ad52e
priyankachennupati <40577708+priyankachennupati@users.noreply.github.com>
2018-08-01 23:04:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ for i in 'cat IPs'
 do
   echo "Deploying war file into $i QA server.."
   sleep 3
-  sshpass -p 'priya' scp target/ola.war priya@$i:/home/priya/softwares/apache-tomcat-7.0.88/webapps
+  sshpass -p "priya" scp target/ola.war priya@$i:/home/priya/softwares/apache-tomcat-7.0.88/webapps
   echo "starting tomcat server in $i QA server.."
-  sshpass -p 'priya' ssh priya@$i "JAVA_HOME=/home/priya/softwares/jdk1.8.0_172" "/home/priya/softwares/apache-tomcat-7.0.88/bin/./startup.sh"
+  sshpass -p "priya" ssh priya@$i "JAVA_HOME=/home/priya/softwares/jdk1.8.0_172" "/home/priya/softwares/apache-tomcat-7.0.88/bin/./startup.sh"
 done
  echo "deploy success"
 fi
